ade3f73ade3e9715ba59472c3ae8d84fd72e7c0a
stevexy <seayoung@gmail.com>
2018-05-25 23:11:02 +0800

    first

---
+++ url.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env Python
+# coding=utf-8
+"""
+the url structure of website
+"""
+
+import sys     #utf-8£¬¼æÈÝºº×Ö
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+#from handlers.index import IndexHandler
+
+url = [
+#    (r'/', IndexHandler),
+]
